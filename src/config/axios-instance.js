6e0113847bec2d89e9fb9ae3a133d88f14200c5c
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-09-25 17:32:43 +0500

    bugs fixed

--- src/config/axios-instance.js
+++ src/config/axios-instance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log('urls: ',import.meta.env.VITE_FULL_URL, import.meta.env.VITE_BASE_URL);
-
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_FULL_URL,
 });
